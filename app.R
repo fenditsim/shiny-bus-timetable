5880d7eac5a8b2e997037dde3372e68b4958742c
Fendi <phd19tt@mail.wbs.ac.uk>
2024-07-30 14:04:11 +0100

    v0.2 - Updated layout with narbarPage instead of dashboardPage - Updated inputs with VirtualSelectInput() instead of SelectInput() - Updated Info tab for reporting any issues found

--- app.R
+++ app.R
@@ -4,93 +4,81 @@ library(shinythemes)
 library(shinydashboard)
 library(shinyjs)
 library(shinyBS)
+library(shinyWidgets)
 
 # Import relevant R scripts
 source("data.R")
 source("tabInfo.R")
 source("tabLog.R")
+#source("https://raw.githubusercontent.com/dreamRs/shinyWidgets/26838f9e9ccdc90a47178b45318d110f5812d6e1/R/useShinydashboard.R")  # In case when useShinyDashboard() is depreciated
 
 # Shiny app
 shinyApp(
   ui = tagList(
     useShinyjs(),
-    dashboardPage(
-      skin = "purple",
-      dashboardHeader(title = "UoW Bus TimeTable"),
-      dashboardSidebar(
-        disable = FALSE,
-        collapsed = TRUE,
-        sidebarMenu(
-          menuItem("TimeTable", tabName = "timetable"),
-          menuItem("About", tabName = "info"),
-          menuItem("Version Log", tabName = "log")
-        )
-      ),
-      dashboardBody(
-        tabItems(
-          tabItem(tabName = "timetable",
-                  # Inputs for origin and destination
-                  fluidPage(
-                    mainPanel(
-                      width = 10,
-                      fluidRow(
-                        selectInput(inputId = "origin",
-                                    label = "From: ",
-                                    choices = stops,
-                                    selected = "Pool Meadow Bus Station",
-                                    width = "100%"
-                        )
-                      ),
-                      fluidRow(
-                        selectInput(inputId = "dest",
-                                    label = "To: ",
-                                    choices = stops,
-                                    selected = "University of Warwick Bus Interchange",
-                                    width = "100%"
-                        )
-                      ),
-                      # Latest bus arrival time
-                      fluidRow(
-                          div(h1("12x"),
-                              style="min-width:20px;max-width:50%; float:left"
-                              ),
-                          div(div(id = "click.12x", valueBoxOutput(outputId = "latest.12x", width = 12)),
-                              style="min-width:240px;max-width:100%; float:right"
-                              ),
-                      ),
-                      hr(),
-                      fluidRow(
-                        div(h1("11"),
-                            style="min-width:20px;max-width:50%; float:left"
-                        ),
-                        div(div(id = "click.11", valueBoxOutput(outputId = "latest.11", width = 12)),
-                            style="min-width:240px;max-width:100%; float:right"
-                        ),
-                      )
+    useShinydashboard(),
+    navbarPage(
+      theme = "mytheme.css",
+      title = "UoW Bus TimeTable", 
+      collapsible = TRUE,
+      tabPanel(title = "Timetable",
+              # Inputs for origin and destination
+              fluidPage(
+                mainPanel(
+                  width = 10,
+                  fluidRow(
+                    virtualSelectInput(inputId = "origin",
+                                       label = "From: ",
+                                       choices = stops,
+                                       selected = "Coventry Pool Meadow",
+                                       width = "100%",
+                                       search = TRUE
+                                       )
+                  ),
+                  fluidRow(
+                    virtualSelectInput(inputId = "dest",
+                                       label = "To: ",
+                                       choices = stops,
+                                       selected = "University of Warwick Bus Int",
+                                       width = "100%",
+                                       search = TRUE
                     )
+                  ),
+                  # Latest bus arrival time
+                  fluidRow(
+                      div(h1("12x"),
+                          style="min-width:20px;max-width:50%; float:left"
+                          ),
+                      div(div(id = "click.12x", shinydashboard::valueBoxOutput(outputId = "latest.12x", width = 12)),
+                          style="min-width:280px;max-width:100%; float:right"
+                          ),
+                  ),
+                  fluidRow(
+                    div(h1("11"),
+                        style="min-width:20px;max-width:50%; float:left"
+                    ),
+                    div(div(id = "click.11", valueBoxOutput(outputId = "latest.11", width = 12)),
+                        style="min-width:280px;max-width:100%; float:right"
+                    ),
                   )
-          ),
-          tabInfo,
-          tabLog
-        )
-      )
+                )
+              )
+      ),
+      tabPanel(title = "Info", tabInfo),
+      tabPanel(title = "Log", tabLog)
     )
   ),
   server = function(input, output, session){
     
     # update choices for dest
     observe({
-      x <- input$origin
-      updateSelectInput(session,
-                        "dest",
-                        label = "To: ",
-                        choices = if (x != "University of Warwick Bus Int") c("University of Warwick Bus Interchange"="University of Warwick Bus Int") else stops[!stops %in% c("University of Warwick Bus Int", "Quadrant")],
-                        selected = if (x != "University of Warwick Bus Int") c("University of Warwick Bus Interchange"="University of Warwick Bus Int") else c("Pool Meadow Bus Station"="Coventry Pool Meadow")
-      )
+      req(input$origin)
+      updateVirtualSelect(inputId = "dest", label = "To: ", choices = if(input$origin != "University of Warwick Bus Int") "University of Warwick Bus Interchange" else stops[!stops %in% c("University of Warwick Bus Int", "Quadrant")], 
+                          selected = if (input$origin != "University of Warwick Bus Int") "University of Warwick Bus Interchange" else "Coventry Pool Meadow")
     })
     
     # Create reactive objects based on origin and dest
-    start <- reactive({ input$origin })
+    start <- reactive({ if (input$origin == "University of Warwick Bus Interchange") "University of Warwick Bus Int" else input$origin })
     end <- reactive({ if (input$origin == "University of Warwick Bus Int") { if (input$dest %in% c("Canley Prior Deram Walk", "Kenilworth Clock", "Leamington Spa Upper Parade", "Leamington Spa High Street")) input$dest else "University of Warwick Bus Int" } else "Uni" })
     end.origin <- reactive({ input$dest })
     check <- reactive({ list(start(), end(), end.origin()) })
